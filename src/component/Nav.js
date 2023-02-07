fa5f4d33e40ab29638cd110a07d574639d8ac845
Kapelu <ddaniel_calderon@hotmail.com>
2023-02-07 00:05:57 -0300

    upDate 07/02/23

---
+++ src/component/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+	return (
+		<nav className='nav'>
+			<h1>Mi sitio</h1>
+			<ul>
+				<li>Inicio</li>
+				<li>Acerca de</li>
+				<li>Contacto</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Nav
