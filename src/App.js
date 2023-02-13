e87166425a6909a292e92c08c4f04f5835e4676e
Kapelu <ddaniel_calderon@hotmail.com>
2023-02-13 19:16:26 -0300

    upDate 13/02/23

--- src/App.js
+++ src/App.js
@@ -1,37 +1,79 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css'
+
+const Navbar = () => (
+	<nav className='navbar'>
+		<ul>
+			<li>
+				<Link to='/'>Home</Link>
+			</li>
+			<li>
+				<Link to='/section1'>Section 1</Link>
+			</li>
+			<li>
+				<Link to='/section2'>Section 2</Link>
+			</li>
+			<li>
+				<Link to='/section3'>Section 3</Link>
+			</li>
+			<li>
+				<Link to='/section4'>Section 4</Link>
+			</li>
+			<li>
+				<Link to='/section5'>Section 5</Link>
+			</li>
+		</ul>
+	</nav>
+)
+
+const Home = () => (
+	<div className='home'>
+		<h1>Home</h1>
+	</div>
+)
+
+const Section1 = () => (
+	<div className='section section1'>
+		<h1>Section 1</h1>
+	</div>
+)
+
+const Section2 = () => (
+	<div className='section section2'>
+		<h1>Section 2</h1>
+	</div>
+)
+
+const Section3 = () => (
+	<div className='section section3'>
+		<h1>Section 3</h1>
+	</div>
+)
+
+const Section4 = () => (
+	<div className='section section4'>
+		<h1>Section 4</h1>
+	</div>
+)
+
+const Section5 = () => (
+	<div className='section section5'>
+		<h1>Section 5</h1>
+	</div>
+)
 
 function App() {
 	return (
-		<div className='App'>
-			<nav className='App-nav'>
-				<ul>
-					<li>
-						<Link to='#section1'>Sección 1</Link>
-					</li>
-					<li>
-						<Link to='#section2'>Sección 2</Link>
-					</li>
-					<li>
-						<Link to='#section3'>Sección 3</Link>
-					</li>
-					<li>
-						<Link to='#section4'>Sección 4</Link>
-					</li>
-					<li>
-						<Link to='#section5'>Sección 5</Link>
-					</li>
-				</ul>
-			</nav>
-			<main className='App-main'>
-				<section id='section1' className='App-section'></section>
-				<section id='section2' className='App-section'></section>
-				<section id='section3' className='App-section'></section>
-				<section id='section4' className='App-section'></section>
-				<section id='section5' className='App-section'></section>
-			</main>
-		</div>
+		<Router>
+			<Navbar />
+			<Route exact path='/' component={Home} />
+			<Route path='/section1' component={Section1} />
+			<Route path='/section2' component={Section2} />
+			<Route path='/section3' component={Section3} />
+			<Route path='/section4' component={Section4} />
+			<Route path='/section5' component={Section5} />
+		</Router>
 	)
 }
 
