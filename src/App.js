477e291433babc720d351660578d4118a5d4a5c8
Kapelu <ddaniel_calderon@hotmail.com>
2023-02-15 23:26:52 -0300

    upDate 15/02/23

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ export default function App() {
 			<header className='nav'>
 				<nav className='nav__container__actions'>
 					<ul>
-						<li><Link activeClass='active' smooth spy to='about'>HOME</Link></li>
-						<li><Link activeClass='active' smooth spy to='projects'>ABOUT</Link></li>
-						<li><Link activeClass='active' smooth spy to='blog'>PROJECTS</Link></li>
+						<li><Link activeClass='active' smooth spy to='home'>HOME</Link></li>
+						<li><Link activeClass='active' smooth spy to='about'>ABOUT</Link></li>
+						<li><Link activeClass='active' smooth spy to='projects'>PROJECTS</Link></li>
 						<li><Link activeClass='active' smooth spy to='contact'>CONTACT</Link></li>
 					</ul>
 				</nav>
 			</header>
-			<section id='about'>Home</section>
-			<section id='blog'>About</section>
+			<section id='home'>Home</section>
+			<section id='about'>About</section>
 			<section id='projects'>Projects</section>
 			<section id='contact'>Contact</section>
 		</div>
