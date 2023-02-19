e189f69b6df27a0cb8d101a6da13823f19b8c901
Kapelu <ddaniel_calderon@hotmail.com>
2023-02-19 20:47:07 -0300

    upDate 19/02/23

--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -1,13 +1,30 @@
 const Portfolio = () => {
-    return(
-        <div>
-            <h1>View Portfolio</h1>
-            <p>
-                Vivamus arcu felis bibendum ut. Tincidunt id aliquet risus feugiat in ante metus dictum. Volutpat maecenas volutpat blandit aliquam. Tempor orci eu lobortis elementum nibh. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Rhoncus mattis rhoncus urna neque viverra. Quisque id diam vel quam. Purus semper eget duis at tellus at. Ultricies leo integer malesuada nunc. Lectus sit amet est placerat in egestas erat imperdiet. Ut consequat semper viverra nam libero. Hendrerit dolor magna eget est. Diam vulputate ut pharetra sit amet aliquam id diam maecenas.
-
-Cursus in hac habitasse platea dictumst. Dolor purus non enim praesent elementum facilisis leo. Dignissim suspendisse in est ante in nibh mauris cursus mattis. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Dolor magna eget est lorem ipsum dolor. In cursus turpis massa tincidunt dui ut ornare lectus sit. Libero nunc consequat interdum varius sit. Turpis egestas sed tempus urna et pharetra pharetra massa. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare.
-				</p>
-        </div>
-    )
+	return (
+		<div>
+			<h1>View Portfolio</h1>
+			<p>
+				Vivamus arcu felis bibendum ut. Tincidunt id aliquet risus
+				feugiat in ante metus dictum. Volutpat maecenas volutpat blandit
+				aliquam. Tempor orci eu lobortis elementum nibh. Commodo odio
+				aenean sed adipiscing diam donec adipiscing tristique. Rhoncus
+				mattis rhoncus urna neque viverra. Quisque id diam vel quam.
+				Purus semper eget duis at tellus at. Ultricies leo integer
+				malesuada nunc. Lectus sit amet est placerat in egestas erat
+				imperdiet. Ut consequat semper viverra nam libero. Hendrerit
+				dolor magna eget est. Diam vulputate ut pharetra sit amet
+				aliquam id diam maecenas. Cursus in hac habitasse platea
+				dictumst. Dolor purus non enim praesent elementum facilisis leo.
+				Dignissim suspendisse in est ante in nibh mauris cursus mattis.
+				Mi quis hendrerit dolor magna eget est lorem ipsum dolor.
+				Consequat ac felis donec et odio pellentesque diam volutpat
+				commodo. Dolor magna eget est lorem ipsum dolor. In cursus
+				turpis massa tincidunt dui ut ornare lectus sit. Libero nunc
+				consequat interdum varius sit. Turpis egestas sed tempus urna et
+				pharetra pharetra massa. Pellentesque eu tincidunt tortor
+				aliquam nulla facilisi cras fermentum. Mattis ullamcorper velit
+				sed ullamcorper morbi tincidunt ornare.
+			</p>
+		</div>
+	)
 }
 export default Portfolio
